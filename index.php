b742b7fb678e9d83f7e9cad302c0f4a5d60d5804
Bryhan Chise Teran <u201315162@upc.edu.pe>
2017-05-04 13:38:58 +0000

    123

--- index.php
+++ index.php
@@ -1,21 +1,67 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+ <html>
+ <head>
+  <title>PHP Starter Application</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" href="style.css" />
+ </head>
+ <body>
+  
+  <div class="topnav">
+	   <a class="active" href="#contacto">Contacto</a>
+  	   <a href="#celebrityburger">Celebrity Burger</a>
+       <a href="#eventos">Eventos</a>
+       <a href="#sucursales">Sucursales</a>
+       <a href="#gintonics">Gin Tonics</a>
+       <a href="#menu">Menu</a>
+  </div>
+  
+  <h1 style="text-align:center;"><img src="images/ImagenCentral.jpg" alt="Polanco"/></h1>
+  
+  <table>
+  
+  <tr>
+  	<th>EMPLOYEE_ID</th>
+  	<th>FIRST_NAME</th>
+  	<th>LAST_NAME</th>
+  	<th>EMAIL</th>
+  </tr>
+  
+  
+  
+   <?php
+  $servername = "us-cdbr-iron-east-03.cleardb.net";
+  $username = "b858dd45b1b222";
+  $password = "3be21204";
+  $dbname = "ad_d51f7af0c3bff3d";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT * from employee";
+  $result = $conn->query($sql);
+  
+  
+  
+  if ($result->num_rows > 0) {
+      // output data of each row
+      
+      
+      while($row = $result->fetch_assoc()) {?>
+      <tr>
+	      <td><?php echo $row['EMPLOYEE_ID']?></td>
+	      <td><?php echo $row['FIRST_NAME']?></td>
+	      <td><?php echo $row['LAST_NAME']?></td>
+	      <td><?php echo $row['EMAIL']?></td>
+      </tr>
+     <?php }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+ ?> 
+ </table>
+ </body>
+ </html>
